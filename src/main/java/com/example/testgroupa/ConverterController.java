8b319fd0553cb56512cc3a6cd9705aebf2a825c9
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-22 13:45:47 +0200

    Test1

--- src/main/java/com/example/testgroupa/ConverterController.java
+++ src/main/java/com/example/testgroupa/ConverterController.java
@@ -9,10 +9,10 @@ public class ConverterController {
     Converter converter = new Converter();
 
     @RequestMapping("/sum")
-    public int sum(
+   // public int sum(
             @RequestParam int a,
             @RequestParam int b) {
         //is just a basic return converter.sum(a, b);
 
-    }
+ //   }
 }
