db9e3a41af0035cd305f9773c2d8295992f7d9e4
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-22 14:18:49 +0200

    develop einfügen

--- src/main/java/com/example/testgroupa/ConverterController.java
+++ src/main/java/com/example/testgroupa/ConverterController.java
@@ -1,7 +1,7 @@
 package com.example.testgroupa;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
-
+//test
 @RestController
 @CrossOrigin
 public class ConverterController {
