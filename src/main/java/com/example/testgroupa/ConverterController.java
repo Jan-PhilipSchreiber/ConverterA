63d33fad43926de063484392096b4b893d84421c
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-22 14:16:46 +0200

    User Funktionen 1 und 2, branches

--- src/main/java/com/example/testgroupa/ConverterController.java
+++ src/main/java/com/example/testgroupa/ConverterController.java
@@ -7,6 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConverterController {
 
     Converter converter = new Converter();
+    @RequestMapping("/cm")
+    //convertes inches to cm
+    //https://www.google.com/search?client=firefox-b-d&q=how+to+convert+inches+to+cm
+    public double cm(
+            @RequestParam double a) {
+        return converter.cm(a);
 
+    }
+ @RequestMapping("/m")
+    //convertes yards to meter
+    //https://www.google.com/search?client=firefox-b-d&q=how+to+convert+yard+to+meter
+    public double m(
+    @RequestParam double a) {
+    return converter.m(a);
+
+    }
 
 }
