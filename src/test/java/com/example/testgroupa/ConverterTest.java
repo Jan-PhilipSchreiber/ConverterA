8639d44f33615abb9785523ad0bac014fe9c2b7c
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-22 14:41:19 +0200

    Test infrastruktur erstellt

---
+++ src/test/java/com/example/testgroupa/ConverterTest.java
@@ -0,0 +1,16 @@
+package com.example.testgroupa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void cm() {
+    }
+
+    @Test
+    void m() {
+    }
+}
---
+++ src/test/java/com/example/testgroupa/TestGroupAApplicationTest.java
@@ -0,0 +1,11 @@
+package com.example.testgroupa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestGroupAApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
