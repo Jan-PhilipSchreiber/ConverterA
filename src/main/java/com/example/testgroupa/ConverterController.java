a839be46e5cbe1f6cf5ed57df4e4c469e2f0c161
JanSchreiber <wi21b118@technikum-wien.at>
2022-06-22 13:50:20 +0200

    cLEANUP FOR MASTER

--- src/main/java/com/example/testgroupa/ConverterController.java
+++ src/main/java/com/example/testgroupa/ConverterController.java
@@ -8,11 +8,5 @@ public class ConverterController {
 
     Converter converter = new Converter();
 
-    @RequestMapping("/sum")
-   // public int sum(
-            @RequestParam int a,
-            @RequestParam int b) {
-        //is just a basic return converter.sum(a, b);
 
- //   }
 }
